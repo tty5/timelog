f0be48d5db5a463b4c22051e8ca7a54dfd9c4719
tty5 <26674599+tty5@users.noreply.github.com>
2020-09-02 15:18:00 +0800

    Update tmlog.go

--- tmlog.go
+++ tmlog.go
@@ -1,7 +1,9 @@
 package tmlog
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
+	"os"
 	"time"
 )
 
@@ -13,6 +15,13 @@ func init() {
 	xlog := logrus.New()
 	xlog.Formatter = &logrus.TextFormatter{TimestampFormat: time.StampMilli, FullTimestamp: true}
 	log = xlog.WithField("mod", modName)
+
+	f, err := os.OpenFile("/var/tmlog", os.O_APPEND | os.O_CREATE | os.O_RDWR, 0666)
+    if err != nil {
+        fmt.Printf("error opening file: %v", err)
+		return
+    }
+    xlog.SetOutput(f)
 }
 
 func GetLogger() *logrus.Entry {
