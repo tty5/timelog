3e6cae4cc6441c2048948ce6b45b6e03de90a02a
tty5 <26674599+tty5@users.noreply.github.com>
2020-09-03 10:06:57 +0800

    Update tmlog.go

--- tmlog.go
+++ tmlog.go
@@ -11,19 +11,22 @@ var log *logrus.Entry
 
 const modName = "[tmlog]"
 
+var xlog *logrus.Logger
+
 func init() {
-	xlog := logrus.New()
+	xlog = logrus.New()
 	xlog.Formatter = &logrus.TextFormatter{TimestampFormat: time.StampMilli, FullTimestamp: true}
 	log = xlog.WithField("mod", modName)
-
-	f, err := os.OpenFile("/var/tmlog", os.O_APPEND | os.O_CREATE | os.O_RDWR, 0666)
-    if err != nil {
-        fmt.Printf("error opening file: %v", err)
-		return
-    }
-    xlog.SetOutput(f)
 }
 
 func GetLogger() *logrus.Entry {
 	return log
+}
+
+func SetLogPath(path string) {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		panic(fmt.Sprintln("tmlog error opening file: %v", err))
+	}
+	xlog.SetOutput(f)
 }
