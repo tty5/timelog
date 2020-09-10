05b11c1d91827b21042b465c42c85e154cf75f1e
tty5 <26674599+tty5@users.noreply.github.com>
2020-09-10 11:26:44 +0800

    Update tmlog.go

--- tmlog.go
+++ tmlog.go
@@ -4,29 +4,49 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"os"
+	"sync"
 	"time"
 )
 
-var log *logrus.Entry
+var gloge *logrus.Entry
 
 const modName = "[tmlog]"
 
-var xlog *logrus.Logger
+var glog *logrus.Logger
 
 func init() {
-	xlog = logrus.New()
-	xlog.Formatter = &logrus.TextFormatter{TimestampFormat: time.StampMilli, FullTimestamp: true}
-	log = xlog.WithField("mod", modName)
+	glog = logrus.New()
+	glog.Formatter = &logrus.TextFormatter{TimestampFormat: time.StampMilli, FullTimestamp: true}
+	gloge = glog.WithField("mod", modName)
 }
 
-func GetLogger() *logrus.Entry {
-	return log
+func GetLg() *logrus.Entry {
+	return gloge
 }
 
-func SetLogPath(path string) {
+var lgMap = make(map[string]*logrus.Logger)
+var lgMapLock sync.RWMutex
+
+func GetLgWithPath(path string) *logrus.Logger {
+	lgMapLock.RLock()
+	if g, ok := lgMap[path]; ok {
+		lgMapLock.RUnlock()
+		return g
+	}
+	lgMapLock.RUnlock()
+
+	l := logrus.New()
+	l.Formatter = &logrus.TextFormatter{TimestampFormat: time.StampMilli, FullTimestamp: true}
+
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
 		panic(fmt.Sprintln("tmlog error opening file: %v", err))
 	}
-	xlog.SetOutput(f)
+	l.SetOutput(f)
+
+	lgMapLock.Lock()
+	lgMap[path] = l
+	lgMapLock.Unlock()
+
+	return l
 }
